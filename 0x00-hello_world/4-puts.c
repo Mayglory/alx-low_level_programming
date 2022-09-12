d59be708c083e04254a15f8f5f0ad12060e615a7
Mayglory <maytoglory@gmail.com>
2022-09-12 13:14:40 -0700

    C-programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,20 @@
 #include <studio.h>
 
 /**
-*main - Entry point
-*
-*Return: Always 0
-*/
+ * main - Entry point
+ *
+ * Author: Mayglory
+ *
+ *Return: Always 0 (success)
+ */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
+        char string[70] = "\"Programming is like building a multilingual puzzle"";
 
-return (0);
+	puts(string);
+
+	/* Tried printing it */
+
+	puts("\"programming is like a multilingual puzzle\"");
+        return (0);
 }
