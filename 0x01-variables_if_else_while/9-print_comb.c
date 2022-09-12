ae61630e8727bee7430918e51a06b30c4596be05
Mayglory <maytoglory@gmail.com>
2022-09-12 16:11:56 -0700

    C-programming variables

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * author:sammy
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int sammy;
+
+	for (sammy = 48; sammy < 58; sammy++)
+	{
+		putchar(sammy);
+		if (sammy != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
