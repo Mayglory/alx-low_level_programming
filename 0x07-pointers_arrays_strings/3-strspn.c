dfe7b1ba590108e968b597d1abf8589665134633
Mayglory <maytoglory@gmail.com>
2022-09-27 14:33:43 -0700

    more pointers

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * _strspn - Gets the length of preattached substring.
- * s: The string to be searched.
- * accept: The prefix to be measured.
+ * _strspn - Gets the length of a prefix substring.
+ * @s: The string to be searched.
+ * @accept: The prefix to be measured.
  *
  * Return: The number of bytes in s which
  * consist only of bytes from accept
